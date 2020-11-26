4bcccc9be01b95afed12d08c4f96e1959f68190c
marty <arno.marty@etu.u-bordeaux.fr>
2020-11-26 20:52:56 +0100

    La physique de jeu s'applique aux monstres

--- src/main/java/fr/ubx/poo/model/go/character/Monster.java
+++ src/main/java/fr/ubx/poo/model/go/character/Monster.java
@@ -5,6 +5,10 @@ import fr.ubx.poo.game.Game;
 import fr.ubx.poo.game.Position;
 import fr.ubx.poo.model.MonsterTimer;
 import fr.ubx.poo.model.Movable;
+import fr.ubx.poo.model.decor.Box;
+import fr.ubx.poo.model.decor.Decor;
+import fr.ubx.poo.model.decor.Stone;
+import fr.ubx.poo.model.decor.Tree;
 import fr.ubx.poo.model.go.GameObject;
 
 import java.util.Timer;
@@ -27,7 +31,7 @@ public class Monster extends GameObject implements Movable {
         timer = new Timer();
         moveHandler = new MonsterTimer(this);
         //timer.scheduleAtFixedRate(() -> requestMove(Direction.random()), 1000, 1000);
-        timer.scheduleAtFixedRate(moveHandler, 1000, 1000 );
+        timer.scheduleAtFixedRate(moveHandler, 1000, 400 );
     }
 
     public void requestMove(Direction direction) {
@@ -39,7 +43,12 @@ public class Monster extends GameObject implements Movable {
 
     @Override
     public boolean canMove(Direction direction) {
-        return true;
+        Position nextPos = direction.nextPosition(getPosition());
+        Decor nextDecor = game.getWorld().get(nextPos);
+
+        return (game.getWorld()).isInside(nextPos)
+                && !(nextDecor instanceof Decor);
+
     }
 
     @Override
